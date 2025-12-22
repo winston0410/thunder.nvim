cfc2a718bb3df3d7e694c8ad09b8ba77bfcb8bd6
nobody <hugosum.dev@protonmail.com>
2025-12-22 10:35:38 +0000

    first working example

--- lua/thunder/init.lua
+++ lua/thunder/init.lua
@@ -1,7 +1,8 @@
--- TODO Use vim.pos to improve code quality https://github.com/neovim/neovim/issues/25509
 local M = {}
 local THUNDER_NS = vim.api.nvim_create_namespace('thunder')
-local _unused_labels = ''
+local ESC_KEY = vim.api.nvim_replace_termcodes("<esc>", true, true, true)
+local unused_labels = ''
+local label_pos = {}
 
 ---@class Thunder.Config
 local default_opts = {
@@ -10,7 +11,7 @@ local default_opts = {
     before = true,
     after = false,
     uppercase = true,
-    style = 'inline',
+    style = 'overlay',
   },
   highlight = {
     priority = 5000,
@@ -27,14 +28,14 @@ end
 
 ---@return string An unused label from the label pool
 local function get_next_label()
-  local next = _unused_labels:sub(1, 1)
-  _unused_labels = _unused_labels:sub(2)
+  local next = unused_labels:sub(1, 1)
+  unused_labels = unused_labels:sub(2)
   return next
 end
 
 M.setup = function(opts)
   M.options = vim.tbl_deep_extend('force', {}, default_opts, opts or {})
-  _unused_labels = M.options.labels
+  unused_labels = M.options.labels
 
   local links = {
     FlashBackdrop = 'Comment',
@@ -50,17 +51,6 @@ M.setup = function(opts)
   end
 
   local group = vim.api.nvim_create_augroup('thunder', { clear = true })
-  vim.api.nvim_create_autocmd('CmdlineChanged', {
-    group = group,
-    callback = function()
-      if not is_search() then
-        return
-      end
-      -- M.reset()
-      -- M.update()
-    end,
-  })
-
   vim.api.nvim_create_autocmd('CmdlineLeave', {
     group = group,
     callback = function()
@@ -71,9 +61,6 @@ M.setup = function(opts)
         return
       end
       M.update()
-        vim.on_key(function (key)
-            print("key found", key)
-        end)
     end,
   })
   vim.api.nvim_create_autocmd('CmdlineEnter', {
@@ -113,32 +100,52 @@ local function get_all_matches(pattern)
 end
 
 M.update = function()
+  local win = vim.api.nvim_get_current_win()
   local pattern = vim.fn.getcmdline()
   -- REF https://github.com/folke/flash.nvim/blob/fcea7ff883235d9024dc41e638f164a450c14ca2/lua/flash/plugins/search.lua#L51
   if pattern:sub(1, 1) == vim.fn.getcmdtype() then
     pattern = vim.fn.getreg('/') .. pattern:sub(2)
   end
   local matches = get_all_matches(pattern)
+  if #matches == 1 then
+      return
+  end
   for _, match in ipairs(matches) do
     local label = get_next_label()
     if label == '' then
       break
     end
-    local row = match[1] - 1
-    local col = match[2] - 1
-    vim.api.nvim_buf_set_extmark(0, THUNDER_NS, row, col, {
+    local pos = vim.pos(match[1] - 1, match[2] - 1)
+
+    label_pos[label] = pos:to_cursor()
+    local extmark_pos = pos:to_extmark()
+    vim.api.nvim_buf_set_extmark(0, THUNDER_NS, extmark_pos[1], extmark_pos[2], {
       priority = M.options.highlight.priority,
       virt_text = { { label, 'FlashLabel' } },
       virt_text_pos = M.options.label.style,
-      end_col = col - 1,
-      strict = false,
+      strict = true
     })
   end
+  vim.cmd.redraw()
+  local ok, ret = pcall(vim.fn.getcharstr)
+  if not ok or ret == ESC_KEY then
+      vim.api.nvim_buf_clear_namespace(0, THUNDER_NS, 0, -1)
+      return
+  end
+  local cursor_pos = label_pos[ret]
+  vim.api.nvim_buf_clear_namespace(0, THUNDER_NS, 0, -1)
+  if cursor_pos == nil then
+      return
+  end
+  -- NOTE run cursor setting in next event loop, as the cursor setting for search result will happen in this loop, and we cannot stop it
+  vim.schedule(function()
+      vim.api.nvim_win_set_cursor(win, cursor_pos)
+  end)
 end
 
 M.reset = function()
-  vim.api.nvim_buf_clear_namespace(0, THUNDER_NS, 0, -1)
-  _unused_labels = M.options.labels
+  unused_labels = M.options.labels
+  label_pos = {}
 end
 
 return M
